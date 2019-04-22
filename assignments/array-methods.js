0694473ef1eb4c00ada68d242e603f5242319593
Nathan Griffith <nathangriffith@Nathans-MacBook-Pro.local>
2019-04-22 12:41:52 -0400

    finished project

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,21 +56,64 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+let fL = () => {
+    runners.forEach(function(runner){
+       return fullName.push(`${runner.first_name} ${runner.last_name}`)
+    })
+};
+fL()
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+let cFirstNames = () => {
+    runners.map(function(firstName){
+
+       return allCaps.push(firstName.first_name.toUpperCase())
+    
+       
+    })
+}
+
+cFirstNames()
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+const LGS = runners.filter(function (shirt){
+    if (shirt.shirt_size === "L"){
+       return largeShirts.push(`${shirt.first_name} ${shirt.last_name} Due to an ordering issue you need to select a shirt size other than LG`)
+    }
+})
+
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+
+
+let sum = []
+const seperateNums =  runners.forEach(function(donate){
+             
+             sum.push(donate.donation)
+
+                   
+    return ticketPriceTotal = sum.reduce(function(a,b){
+        return a+b;
+    })
+        
+                   
+    })
+ 
+
+
+
+
+
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,16 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function makeFunc() {
+  let name = 'dog';
+  function displayName() {
+    alert(name);
+  }
+  return displayName;
+}
 
+let myFunc = makeFunc();
+myFunc();
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
